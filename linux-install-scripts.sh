4b8b6da13270fa47befeb9e108d181800b71a3ef
Homero Barrocas Soares Esmeraldo <homero.esmeraldo@cncb.ox.ac.uk>
2018-04-15 15:30:13 +0100

    Initial Commit with jupyter and notification for caps-lock

---
+++ linux-install-scripts.sh
@@ -0,0 +1,8 @@
+# python notebooks
+pip install jupyter
+pip install jupyterthemes
+
+# notification for caps lock key
+sudo add-apt-repository ppa:tsbarnes/indicator-keylock
+sudo apt-get update
+sudo apt-get install indicator-keylock
