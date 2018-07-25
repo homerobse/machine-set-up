897aa6e5fcad97a1a7c61e55ac576179514e6b66
Homero Barrocas Soares Esmeraldo <homero.esmeraldo@cncb.ox.ac.uk>
2018-07-25 15:00:28 +0100

    Play on Linux and snippets for calculating expressions on bash

--- linux-install-scripts.sh
+++ linux-install-scripts.sh
@@ -6,3 +6,9 @@ pip install jupyterthemes
 sudo add-apt-repository ppa:tsbarnes/indicator-keylock
 sudo apt-get update
 sudo apt-get install indicator-keylock
+
+# install play on linux
+wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
+sudo wget http://deb.playonlinux.com/playonlinux_precise.list -O /etc/apt/sources.list.d/playonlinux.list
+sudo apt-get update
+sudo apt-get install playonlinux
