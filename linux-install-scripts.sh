83afa048cb5323481b5c3b429eb0545d4526032d
Homero Barrocas Soares Esmeraldo <homero.esmeraldo@cncb.ox.ac.uk>
2018-07-26 11:49:56 +0100

    New document right-click option fix

--- linux-install-scripts.sh
+++ linux-install-scripts.sh
@@ -12,3 +12,7 @@ wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
 sudo wget http://deb.playonlinux.com/playonlinux_precise.list -O /etc/apt/sources.list.d/playonlinux.list
 sudo apt-get update
 sudo apt-get install playonlinux
+
+# Create new document right-click option (which is initially missing in Ubuntu 16.04 and 18.04)
+cd ~
+touch ~/Templates/Text\ File.txt
