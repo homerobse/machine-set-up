d144720c28e2c47cae8b763530d351260c7dfd56
Homero Barrocas Soares Esmeraldo <homero.esmeraldo@cncb.ox.ac.uk>
2019-06-25 18:27:27 -0300

    Create file with some useful bashrc scripts

---
+++ bashrc_scripts.sh
@@ -0,0 +1,18 @@
+## Homero preferences ##
+
+export CONDA_CHANGEPS1=false  # to avoid the (base) ahead of my terminal
+# Miniconda3 see https://stackoverflow.com/a/54725966/1273751
+. /home/homero/miniconda3/etc/profile.d/conda.sh
+conda activate
+
+export CONDA_CHANGEPS1=true  # so that it shows (env) ahead of the terminal after the first terminal initialization
+
+# shortcut commands to favourite folders
+alias oxdir='cd ~/hd/Dropbox/oxford'
+alias dgdir='cd ~/dphil/ufrn/dg-cells'
+alias gendir='cd ~/dphil/gen-learn/'
+
+# other shortcuts
+alias jnb='jupyter-notebook'
+alias gst='git status'
+alias gpsh='git push'
