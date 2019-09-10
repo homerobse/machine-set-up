d578895d8d9f5f5372bff95393e02d6188aa3337
Homero Barrocas Soares Esmeraldo <homero.esmeraldo@cncb.ox.ac.uk>
2019-09-10 00:33:58 -0300

    Create python startup script with numpy and matplotlib imports

---
+++ homero_ipython_custom_start.py
@@ -0,0 +1,7 @@
+# this file was added to .ipython/profile_default/startup/
+# as suggested in https://stackoverflow.com/a/11124846/1273751
+from numpy import *
+import numpy as np
+from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
+
