48d0e5e94ff77f1b187722c11e03fbe417f54e95
Homero Barrocas Soares Esmeraldo <homero.esmeraldo@cncb.ox.ac.uk>
2020-08-06 20:07:49 -0300

    Add script with useful commands to free space in linux

---
+++ free_space.sh
@@ -0,0 +1,32 @@
+# some commands taken from https://itsfoss.com/free-up-space-ubuntu-linux/
+
+# remove no longer required packages
+sudo apt-get autoremove
+
+# remove apt cache
+# check how much space will be released
+sudo du -sh /var/cache/apt
+# remove only part of the cache
+sudo apt-get autoclean
+# remove everything
+sudo apt-get clean
+
+# Clear systemd journal logs
+# check how much space will be freed
+journalctl --disk-usage
+# choose lower number of days to keep to delete more
+sudo journalctl --vacuum-time=180d
+
+# to check which packages would be uninstalled if you run the command below
+conda clean --all --dry-run
+# remove packages no longer used
+conda clean --all
+
+
+# to clean linux cache. Change number of days to remove more or less
+# https://askubuntu.com/a/732281/315699
+# to check 
+find ~/.cache/ -depth -type f -atime +365
+# to delete
+find ~/.cache/ -depth -type f -atime +365 -delete
+
