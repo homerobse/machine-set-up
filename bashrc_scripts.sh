06621d9d88988dbb1c4c2009e0cb6e9b557ab06c
Homero Esmeraldo <homero.esmeraldo@gmail.com>
2021-03-23 12:32:17 -0300

    Add bashrc commands useful for WSL

--- bashrc_scripts.sh
+++ bashrc_scripts.sh
@@ -21,3 +21,19 @@ alias gst='git status'
 alias gpsh='git push'
 gcmt () { git commit -m "$1";};
 
+## WSL SPECIFIC
+# set up environment variables for X terminal in WSL
+# sources: https://wiki.ubuntu.com/WSL  https://stackoverflow.com/a/59843603/1273751
+export DISPLAY=localhost:0.0
+export LIBGL_ALWAYS_INDIRECT=1
+export XDG_RUNTIME_DIR=/home/homero/runtimedir
+export RUNLEVEL=3
+
+# set colors for ls command output
+# see https://askubuntu.com/a/466203/315699 for reference of what each number means (style/background color/font color) 
+LS_COLORS='ow=01;90;102'
+export LS_COLORS
+
+# I can't remember how this BROWSER variable appeared here. Not sure if it was me or a program who put it here. Maybe it was to make jupyter notebook automatically open the browser
+export BROWSER="/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+
